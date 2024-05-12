d08cca80b1d018827d233c651a124f2a6ceff2d2
Shikhor Roy <shikhorroy.cse12@gmail.com>
2024-05-12 11:52:37 +0600

    Lecture 02 - Variables in Java

--- src/Lecture01.java
+++ src/Lecture01.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Lecture01 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java B2A Comprehensive Course!");
     }
---
+++ src/Lecture02.java
@@ -0,0 +1,11 @@
+public class Lecture02 {
+    public static void main(String[] args) {
+        int num1 = 500;// initialization
+        int num2 = 50;// initialization
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+    }
+}
