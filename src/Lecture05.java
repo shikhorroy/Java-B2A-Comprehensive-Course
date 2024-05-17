47dd83da76fd4dfca6d7413a5bce5d4bba18c6c0
Shikhor Roy <shikhorroy.cse12@gmail.com>
2024-05-17 08:40:38 +0600

    Lecture 05 - Swap Two Values

---
+++ src/Lecture05.java
@@ -0,0 +1,19 @@
+public class Lecture05 {
+    public static void main(String[] args) {
+        int x = 127;
+        int y = 220;
+
+        // before swapping:
+        System.out.println("Before swapping:");
+        System.out.println("x = " + x + " y = " + y);
+
+        // swapping two values:
+        int tempVar = x;
+        x = y;
+        y = tempVar;
+
+        // after swapping:
+        System.out.println("After swapping:");
+        System.out.println("x = " + x + " y = " + y);
+    }
+}
