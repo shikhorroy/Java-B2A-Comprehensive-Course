a642629791718a4379d408d47cdd7d7b363bb5e3
Shikhor Roy <shikhorroy.cse12@gmail.com>
2024-05-18 23:37:15 +0600

    Lecture 07 - More about Console Input Part 1

---
+++ src/Lecture07.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Lecture 07: More about Console Input Part 1
+ */
+public class Lecture07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // 144 256
+        // 789 6987
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        System.out.println("num1 = " + num1 + " num2 = " + num2);
+
+        char myChar = input.next().charAt(0);
+        System.out.println("myChar = " + myChar);
+
+        String myString = input.next();
+        System.out.println("myString = " + myString);
+    }
+}
