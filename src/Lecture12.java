e3be46bc4c34d7a04baaf6ba5b871566d0e3b222
Shikhor Roy <shikhorroy.cse12@gmail.com>
2024-05-25 17:33:03 +0600

    Lecture 12 - Control Flow or if else Statement

---
+++ src/Lecture12.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Lecture 12: Control Flow/Control Statement
+ */
+public class Lecture12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+        int y = input.nextInt();
+
+        if (x == y) {
+            System.out.println("both are equal");
+        } else if (x > y) {
+            System.out.println("x is bigger");
+        } else {
+            System.out.println("y is bigger");
+        }
+    }
+}
