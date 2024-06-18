9d2186a7fa4e7b6ef18feebe00525414c21c827b
Shikhor Roy <shikhorroy.cse12@gmail.com>
2024-06-18 12:25:34 +0600

    Lecture 18 - Loop in Java (Part 1)

---
+++ src/Lecture18.java
@@ -0,0 +1,29 @@
+/**
+ * Lecture 18: Loop (Part 1) - for loop
+ */
+public class Lecture18 {
+    public static void main(String[] args) {
+
+        // for(initialization; termination/condition; increment/decrement/update) {}
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println("Welcome to Java Programming");
+//        }
+
+        // print: 1 2 3 4 ..... 50
+//        for (int i = 1; i <= 50; i++) {
+//            System.out.println(i);
+//        }
+
+        // reverse order:
+//        for (int i = 50; i >= 1; i--) {
+//            System.out.println(i);
+//        }
+
+        // summation of: 1 + 2 + 3 + 4 + ..... + 100
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
