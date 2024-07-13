2025cf1390c7cd9bdadf56c9d6e0c6339c66ed3b
Shikhor Roy <shikhorroy.cse12@gmail.com>
2024-07-14 02:29:39 +0600

    Lecture 22 - Break Statement in Java

---
+++ src/Lecture22.java
@@ -0,0 +1,15 @@
+/**
+ * Lecture 22: Break Statement in Java
+ */
+public class Lecture22 {
+    public static void main(String[] args) {
+        int i = 1;
+        for (; i <= 10; i++) {
+            System.out.println(i);
+            if(i == 5) {
+                break;
+            }
+        }
+        System.out.println("outside loop: " + i);
+    }
+}
